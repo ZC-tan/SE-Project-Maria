502321d5222c65660b370ec3747e88c299ce3ec8
zzy0202 <57013433+zzy0202@users.noreply.github.com>
2021-06-12 19:39:49 +0800

    login page

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <form action="" class="login">
+      <p>Login</p>
+      <input type="text" placeholder="用户名">
+      <input type="password" placeholder="密码">
+      <input type="submit" class="btn" value="登  录">&nbsp;&nbsp;&nbsp;
+      <router-link to="login/register">
+        <input class="btn" value="  注  册">
+      </router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login"
+}
+</script>
+
+<style >
+
+</style>
