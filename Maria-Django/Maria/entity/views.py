0a43c3ab7386216e204794ad3b0b6a8f7b8923d5
ZC <tanzc2013511@gmail.com>
2021-06-12 20:04:49 +0800

    添加注释

--- Maria-Django/Maria/entity/views.py
+++ Maria-Django/Maria/entity/views.py
@@ -4,13 +4,12 @@ from entity.form import DocForm
 
 # Create your views here.
 def home(request):
+    #如果已经登陆，回到主界面时，要可以查看文档，信息等
     if request.session.get('username'):
         cur_username = request.session['username']
         return render(request,"home.html",context={'username':cur_username})
-    else:
+    else: #否则回到，显示登入注册的主界面
         return render(request,"home.html")
-    # message = 'try'
-    # return render(request,"home.html",context={'username':username})
 
 def register(request):
     if request.method == 'POST':
@@ -52,48 +51,50 @@ def logout(request):
     request.session.flush()
     return render(request, 'home.html', {'message': '注销成功'})
 
-
-def modify_user_info(request):
+#查看用户信息
+def show_user_info(request): #如果登入了
     if request.session.get('username'):
         cur_username = request.session['username']
-        cur_user = User.objects.get(name=cur_username)
+        cur_user = User.objects.get(name=cur_username) #用名字查询用户，显示用户信息（密码，用户名）
+        return render(request, 'show_user_info.html', {'username':cur_username,'password':cur_user.password})
+    else: #如果还没登入，跳转到登入界面（下面多数也是这样的。。。）
+        return redirect('login/')
+
+#修改用户信息（密码）
+def modify_user_info(request):
+    if request.session.get('username'): #如果登入了
+        cur_username = request.session['username']
+        cur_user = User.objects.get(name=cur_username) #用名字查询用户
         if request.method == 'POST':
-            new_pass = request.post.get('new_password')
-            if new_pass == request.post.get('new_password2'):
+            new_pass = request.post.get('new_password') #新密码
+            if new_pass == request.post.get('new_password2'): #确认新密码
                 cur_user.password = new_pass
                 cur_user.save()
             else:
                 return render(request, 'modify_user_info.html', {'username':cur_username,'message': '两次密码不一致'})
         else:
             return render(request, 'modify_user_info.html', {'username':cur_username})
-    else:
-        return redirect('login/') #如果还没登入，跳转到登入界面
+    else:#如果还没登入，跳转到登入界面
+        return redirect('login/') 
 
-
-def show_user_info(request):
-    if request.session.get('username'):
-        cur_username = request.session['username']
-        cur_user = User.objects.get(name=cur_username)
-        return render(request, 'show_user_info.html', {'username':cur_username,'password':cur_user.password})
-    else:
-        return redirect('login/')
-        
+#显示我创建的文档
 def my_docslist(request):
     if request.session.get('username'):
         cur_username = request.session['username']
-        my_docslist = Doc.objects.filter(creator = cur_username)
-        return render(request,'mydocs.html',{'username':cur_username,'my_docslist':my_docslist})
+        my_docslist = Doc.objects.filter(creator = cur_username) #用用户名字对应 creator 名字，查找文档
+        return render(request,'mydocs.html',{'username':cur_username,'my_docslist':my_docslist}) #返回所有找到的文档
     else:
         return redirect('login/')
 
+#创建文档
 def create_doc(request):
     if request.session.get('username'):
         cur_username = request.session['username']
         cur_user = User.objects.get(name=cur_username)
         if request.method == 'POST':
-            form = DocForm(request.POST)
+            form = DocForm(request.POST) #如果POST请求，就代表用户按了submit，则用POST创建DocForm(一种 ModelForm)的表单
             if form.is_valid():
-                newDoc = form.save(commit=False)
+                newDoc = form.save(commit=False) #用DocForm表单存储 Doc 文档类信息
                 newDoc.creator = cur_username
                 newDoc.save()
             return redirect('/mydocs/')
@@ -102,19 +103,20 @@ def create_doc(request):
             return render(request,'create_doc.html',{'doc': docForm})
     else:
         return redirect('login/')
-    
+
+#修改文档
 def edit_doc(request):
     if request.session.get('username'):
         cur_username = request.session['username']
         cur_user = User.objects.get(name=cur_username)
         if request.method == 'POST':
-            form = DocForm(request.POST)
+            form = DocForm(request.POST) #如果POST请求，就代表用户按了submit，则用POST创建DocForm(一种 ModelForm)的表单
             if form.is_valid():
                 newDoc = form.save(commit=False)
                 newDoc.save()
             return redirect('/mydocs/')
         else:
-            doc_id = request.GET.get('id')
+            doc_id = request.GET.get('id') #根绝GET 请求传进来的id，找到对应的文档，并显示
             doc = Doc.objects.filter(id=doc_id).first()
             docForm = DocForm(instance = doc)
             return render(request,'edit_doc.html',{'title':doc.title,'doc': docForm})
